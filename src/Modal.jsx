013a03abb238f544cc186894f34d0b75f2059a29
kapatric <khristopherpatrick@gmail.com>
2022-11-14 14:56:07 -0500

    last bug fix

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -13,7 +13,7 @@ function Modal({ setShowModal, showModal, onClose, modalData }) {
           <h2 className="modal-title">{modalData.name}</h2>
         </div>
         <div className="modal-body">
-          <img src={modalData.flags.png} />
+          <img src={modalData.flags.png} alt=""/>
         </div>
         <div className="modal-footer">
           <h3><ul>
